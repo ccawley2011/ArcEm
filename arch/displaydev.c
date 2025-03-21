6a651435bb99b3c76a1f3ad425c7f6794c085634
Cameron Cawley <ccawley2011@gmail.com>
2025-03-21 14:46:52 +0000

    Make use of common byteswap code when loading Extension ROMs

--- arch/displaydev.c
+++ arch/displaydev.c
@@ -92,8 +92,10 @@ void DisplayDev_VSync(ARMul_State *state)
   (instead of just endian swapping the destination, as suggested above)
 
 */
-void ByteCopy(uint8_t *dest,const uint8_t *src,size_t size)
+void ByteCopy(void *destp,const void *srcp,size_t size)
 {
+  const uint8_t *src = (const uint8_t *)srcp;
+  uint8_t *dest = (uint8_t *)destp;
   if(!size)
     return;
   /* Align source */
@@ -148,8 +150,10 @@ void ByteCopy(uint8_t *dest,const uint8_t *src,size_t size)
   This time we need to do endian swapping on the destination address
 
 */
-void InvByteCopy(uint8_t *dest,const uint8_t *src,size_t size)
+void InvByteCopy(void *destp,const void *srcp,size_t size)
 {
+  const uint8_t *src = (const uint8_t *)srcp;
+  uint8_t *dest = (uint8_t *)destp;
   if(!size)
     return;
   /* Align destination */
